cdffa09357552f442062dd7446769429a6b07004
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-09-20 21:34:57 +0530

    some changes

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -7,9 +7,9 @@ export default ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
     },
     debug: false,
   },
